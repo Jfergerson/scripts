e28b53a6729fb9c18d3781662eccfd083dadb889
john <testmachine@garbo.com>
2016-12-02 13:00:54 -0800

    git config

--- vmsetup.sh
+++ vmsetup.sh
@@ -3,3 +3,5 @@ sudo apt-get install sysstat
 sudo apt-get install rt-tests
 sudo apt-get install vim
 sudo apt-get install ruby
+git config --global user.email = "testing_vm@garbo.com"
+git config --global user.name = "john"
