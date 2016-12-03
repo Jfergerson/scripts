3a7568a189f83368951beff1724248661cf0887d
john <testmachine@garbo.com>
2016-12-02 17:17:06 -0800

    2core

--- ruby_hackbench.rb
+++ ruby_hackbench.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
-`mpstat 1 10000 > output.txt &`
-puts "######make sure mpstat is running"
-puts `ps -ef | grep mpstat`
-var = `ps -ef | grep mpstat | awk '{print $2;}'`
-puts "######running hackbench"
-temp = `hackbench -l 10000`
-`kill #{var.split("\n")[0]}`
-puts "######make sure mpstat is not running"
-puts `ps -ef | grep mpstat`
+for i in 0..10
+	`mpstat -P ALL 1 10000 > output#{i}.txt &`
+	puts "######make sure mpstat is running"
+	puts `ps -ef | grep mpstat`
+	var = `ps -ef | grep mpstat | awk '{print $2;}'`
+	puts "######running hackbench"
+	temp = `hackbench -l 10000`
+	`kill #{var.split("\n")[0]}`
+	puts "######make sure mpstat is not running"
+	puts `ps -ef | grep mpstat`
+end
