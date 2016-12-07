0f8cea9e925d9032c6621a992bec47e3bf1c16fa
john <testmachine@garbo.com>
2016-12-07 01:50:06 -0800

    fixed 3core

--- parser.rb
+++ parser.rb
@@ -3,7 +3,7 @@
 x = 0
 start =0;
 for i in 0 .. 10
-	`tail -n +2 output#{i}.txt | sponge output#{i}.txt`
+#	`tail -n +2 output#{i}.txt | sponge output#{i}.txt`
 	IO.foreach("output#{i}.txt") do |line|
 		if x>2 then
 			for j in 3 .. 11
@@ -12,6 +12,6 @@ for i in 0 .. 10
 			print(line.split()[12],"\n")
 		end
 		x +=1
-		x = 0 if x == 7
+		x = 0 if x == 6
 	end
 end
